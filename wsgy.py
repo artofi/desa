98cf327a82a42557700c162b30cf1da069c447c6
artofi <joniartofipurba@gmail.com>
2025-09-16 19:11:38 +0700

    Update wsgy.py

--- wsgy.py
+++ wsgy.py
@@ -1,5 +1,4 @@
-# wsgi.py
-from app import app
-
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    start_backup_scheduler()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
